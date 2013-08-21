97d07b4559444dcadf858a90b314d46cb0f56cd4
richard bluestein <richard@bluestein.com>
2013-08-21 18:20:25 -0500

    .

---
+++ app/models/teacher.rb
@@ -0,0 +1,27 @@
+require_relative '../../db/config'
+require 'date'
+
+class Teacher < ActiveRecord::Base
+	validates :age, numericality: { :greater_than => 4 }
+	validates :email, uniqueness: true
+	#validates :phone
+	validates_format_of :email, :with => /\w+@\w+.\w{2}/
+	#validates_numericality_of :phone, :only_integer => true
+	validates_format_of :phone, :with => /.*\d{3}.*\d{3}.*\d{4}.*/
+	#/.*\d{3}.*\d{3}.*\d{4}.*/
+
+	def name
+		first_name + " " + last_name
+	end
+
+	def age
+		 diff = Date.today - birthday
+	     age = (diff / 365.25).floor
+	     age.to_i
+	end
+
+		
+
+
+end
+
